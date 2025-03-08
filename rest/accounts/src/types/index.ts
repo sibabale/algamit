b7e2d3e8d3286169e4f26a9e106a6b88ea69e12f
sibabale <sibabalejoja@gmail.com>
2025-03-08 17:37:01 +0200

    refactor: added the NewAccount type

--- rest/accounts/src/types/index.ts
+++ rest/accounts/src/types/index.ts
@@ -1,9 +1,16 @@
-export type BankAccountStatus = 'ACTIVE' | 'CLOSED';
+import { AccountType } from '@prisma/client'
+
+export type BankAccountStatus = 'ACTIVE' | 'CLOSED'
+
+export type NewAccount = {
+    ownerId: string
+    accountType: AccountType
+}
 
 export type BankAccount = {
-    id: number;
-    owner: string;
-    status: BankAccountStatus;
-    balance: number;
-    accountNumber: number;
-};
+    id: number
+    owner: string
+    status: BankAccountStatus
+    balance: number
+    accountNumber: number
+}
