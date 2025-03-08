e9a19c303639b649e5d33c967648d65faef078e4
sibabale <sibabalejoja@gmail.com>
2025-03-08 15:31:09 +0200

    refactor: added types for transactions

---
+++ rest/transactions/src/types/index.ts
@@ -0,0 +1,29 @@
+export type BankCurrency = 'USD' | 'EUR' | 'GBP' | 'ZAR'
+export type BankTransactionType = 'DEPOSIT' | 'WITHDRAWAL' | 'TRANSFER'
+export type BankTransactionStatus = 'PENDING' | 'COMPLETED' | 'FAILED'
+
+export type BankTransaction = {
+    id: number
+    type: BankTransactionType
+    status: BankTransactionStatus
+    amount: number
+    accountId: number
+    accountBalance: number
+    accountCurrency: string
+    destination: {
+        ownerId: number
+        ownerName: string
+        accountNumber: number
+    }
+}
+
+export type NewTransaction = {
+    id: string
+    type: BankTransactionType
+    amount: number
+    accountNumber: number
+    accountCurrency: BankCurrency
+    destination: {
+        accountNumber: number
+    }
+}
