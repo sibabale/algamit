65019c35497e494e963e4c3f18578c3651809fd4
sibabale <sibabalejoja@gmail.com>
2025-03-08 17:36:41 +0200

    refactor: update endpoint params

--- rest/accounts/src/router.ts
+++ rest/accounts/src/router.ts
@@ -1,16 +1,16 @@
-import { Router } from 'express';
-import { closeAccount } from './controllers/delete/delete';
-import { createAccount } from './controllers/create/create';
+import { Router } from 'express'
+import { closeAccount } from './controllers/delete/delete'
+import { createAccount } from './controllers/create/create'
 import { apiRateLimiter } from './utils/index'
-import { updateAccountOwner } from './controllers/update/update';
-import { getAllAccounts, getAccountById } from './controllers/read/read';
+import { updateAccountOwner } from './controllers/update/update'
+import { getAllAccounts, getAccountById } from './controllers/read/read'
 
-const router = Router();
+const router = Router()
 
-router.post('/accounts', apiRateLimiter, createAccount);
-router.get('/accounts', apiRateLimiter, getAllAccounts);
-router.get('/accounts/:accountNumber', apiRateLimiter, getAccountById);
-router.delete('/accounts/:id', apiRateLimiter, closeAccount);
-router.put('/accounts/:id', apiRateLimiter, updateAccountOwner);
+router.post('/accounts', apiRateLimiter, createAccount)
+router.get('/accounts', apiRateLimiter, getAllAccounts)
+router.get('/accounts/:accountNumber', apiRateLimiter, getAccountById)
+router.delete('/accounts/:accountNumber', apiRateLimiter, closeAccount)
+router.put('/accounts/:accountNumber', apiRateLimiter, updateAccountOwner)
 
-export default router;
+export default router
